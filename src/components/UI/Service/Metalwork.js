fb2824db3d493a50666de5b0678e2b19291e2912
glassbarricades01~ <glassbarricades1@list.ru>
2022-08-27 05:52:40 +0300

    add

--- src/components/UI/Service/Metalwork.js
+++ src/components/UI/Service/Metalwork.js
@@ -1,8 +1,19 @@
-import { Title } from "@mantine/core"
+import { Container, Title } from "@mantine/core";
+import ImgText from "../../section/Img-text";
 
 const MetalWork = () => {
-    return (
-        <Title order={3}>Металлообработка</Title>
-    )
-}
+  return (
+    <Container pt="md">
+      <Title align="center" order={2} mb="md">
+        Металлообработка
+      </Title>
+      <ImgText
+        img={"https://via.placeholder.com/300"}
+        text={
+          "На данный момент наше предприятие производит различные металлоизделия методом холодной штамповки, а также занимаемся разработкой проектной документацией с дальнейшим изготовлением различных пресс-форм и штампов. На всю произведенную нами продукцию мы даем гарантию."
+        }
+      />
+    </Container>
+  );
+};
 export default MetalWork;
